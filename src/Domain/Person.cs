7506a88d9ca5e4280b96a80efb6b3d4604220287
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-09-05 14:04:17 +0200

    Implement Person

---
+++ src/Domain/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Person
+    {
+        private string name = "Benjamin";
+
+        public void SayHello(){
+            System.Console.WriteLine("Hello " + name);
+        }
+    }
+}
